d930e37b91e84e20db3b0d004fea397ed7db7b22
Waldston Santana <waldston.ws@gmail.com>
2024-01-18 06:14:42 -0300

    Project created

---
+++ src/main/java/com/waldstonsantana/webservicescomspringboot/WebservicescomspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.waldstonsantana.webservicescomspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicescomspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicescomspringbootApplication.class, args);
+	}
+
+}
