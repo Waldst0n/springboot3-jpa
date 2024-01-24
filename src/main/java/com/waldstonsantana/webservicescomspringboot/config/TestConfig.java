b9363b6776fd19d3f068057558270d0fe3a3c4a6
Waldston Santana <waldston.ws@gmail.com>
2024-01-24 06:39:12 -0300

    Product

--- src/main/java/com/waldstonsantana/webservicescomspringboot/config/TestConfig.java
+++ src/main/java/com/waldstonsantana/webservicescomspringboot/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.waldstonsantana.webservicescomspringboot.config;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.waldstonsantana.webservicescomspringboot.entities.Category;
 import com.waldstonsantana.webservicescomspringboot.entities.Order;
+import com.waldstonsantana.webservicescomspringboot.entities.Product;
 import com.waldstonsantana.webservicescomspringboot.entities.User;
 import com.waldstonsantana.webservicescomspringboot.entities.enums.OrderStatus;
 import com.waldstonsantana.webservicescomspringboot.repositories.CategoryRepository;
 import com.waldstonsantana.webservicescomspringboot.repositories.OrderRepository;
+import com.waldstonsantana.webservicescomspringboot.repositories.ProductRepository;
 import com.waldstonsantana.webservicescomspringboot.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,6 +42,12 @@ public class TestConfig implements CommandLineRunner {
 		Category c2 = new Category(null, "Books");
 		Category c3 = new Category(null, "Computers");
 		
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
@@ -46,11 +58,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		
 		
-		
 
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		order.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 	}
 	
