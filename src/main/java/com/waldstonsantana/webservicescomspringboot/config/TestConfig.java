2213f3fad1decf4997450ab84fefdd6a177c4e54
Waldston Santana <waldston.ws@gmail.com>
2024-01-25 06:52:12 -0300

    many-to-many association with Join Table

--- src/main/java/com/waldstonsantana/webservicescomspringboot/config/TestConfig.java
+++ src/main/java/com/waldstonsantana/webservicescomspringboot/config/TestConfig.java
@@ -48,6 +48,20 @@ public class TestConfig implements CommandLineRunner {
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+	
+		
+		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		p1.getCategories().add(c2);
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c3);
+		p3.getCategories().add(c3);
+		p4.getCategories().add(c3);
+		p5.getCategories().add(c2);
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
@@ -61,8 +75,9 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		order.saveAll(Arrays.asList(o1,o2,o3));
-		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+	
+		
+		
 		
 	}
 	
